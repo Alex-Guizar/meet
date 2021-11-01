9d3481fc120a5e97d1008bee9fc06cfd75de6b0e
Alex Guizar <alexguizar90@gmail.com>
2021-10-31 17:46:20 -0700

    Made scatterchart responsive

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // Packages
 import React, { Component } from 'react';
 import {
-  ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip
+  ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer
 } from 'recharts';
 
 // React-Bootstrap Components
@@ -100,19 +100,24 @@ class App extends Component {
             <WarningAlert text="Current information is being loaded from the cache." /> :
             ''
           }
+
           <CitySearch locations={locations} updateEvents={this.updateEvents} />
+
           <div className="event-header">
             <h1 className="event-header__heading me-3">Events</h1>
             <NumberOfEvents updateEvents={this.updateEvents} />
           </div>
-          <ScatterChart width={800} height={400}
-            margin={{ top: 20, right: 20, bottom: 10, left: 10 }}>
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis type="category" dataKey="city" name="city" />
-            <YAxis type="number" dataKey="number" name="number of events" />
-            <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-            <Scatter data={this.getData()} fill="#8884d8" />
-          </ScatterChart>
+
+          <ResponsiveContainer height={400}>
+            <ScatterChart margin={{ top: 20, right: 20, bottom: 10, left: 10 }}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis type="category" dataKey="city" name="city" />
+              <YAxis type="number" dataKey="number" name="number of events" />
+              <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+              <Scatter data={this.getData()} fill="#8884d8" />
+            </ScatterChart>
+            </ResponsiveContainer>
+
           <EventList events={events} />
         </Container>
 
